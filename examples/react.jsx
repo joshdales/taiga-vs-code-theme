cc89e930b4059b8521821e6a0633be5fe24de44a
Josh Dales <joshdales@pm.me>
2019-12-25 08:39:22 -0500

    Add react jsx example

---
+++ examples/react.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+// Check something
+const WHAT_THIS = "something"
+
+function Component(props) {
+  const foo = `foo ${props.bar}`
+
+  return (
+    <div>
+      Foo = {foo}
+    </div>
+  )
+}
+
+export class App extends React.Component {
+  keepItSecret = () => {
+    console.log("keep it safe")
+  }
+
+  get somethingForYou() {
+    this.keepItSecret()
+    return "Here you go"
+  }
+
+  render() {
+    return (
+      <div>
+        {this.somethingForYou}
+        <Component bar={WHAT_THIS} />
+      </div>
+    )
+  }
+}

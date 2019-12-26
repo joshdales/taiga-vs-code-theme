3a53cdc0d11e791d9c5f1cf4da5b69513d95b895
Josh Dales <joshdales@pm.me>
2019-12-25 22:33:53 -0500

    Some ruby syntax highlighting

---
+++ examples/ruby.rb
@@ -0,0 +1,32 @@
+require 'somthing'
+
+module Cool
+  # Check if cool
+  class Check < Cool
+
+    include Cool
+
+    def errrrr_what(yeah)
+      self.suff
+    end
+
+    def stuff(this:, that: nil)
+      begin
+        this[:yeah]
+      rescue StandardError
+        "this: #{this}, that: #{that}"
+      end
+    end
+
+    def hash_it_out
+      return {
+        "cool" => "I think so",
+        :super_cool => false,
+        coolest: true
+      }
+    end
+
+    def i_should_think_so
+      return unless Something::Amazing && Cool.suerfly
+    end
+end

9c492d33599ec5da92d54047ccb444aed3cd7293
Josh Dales <joshdales@pm.me>
2021-03-13 13:40:29 -0500

    Add values to the build

--- src/templates/buildTheme.js
+++ src/templates/buildTheme.js
@@ -3,11 +3,16 @@ const path = require("path")
 const JSON5 = require("json5")
 const mustache = require("mustache")
 
+const opacity = require('./opacity.json')
+const colours = require('./colours.json')
+
+const themeValues = {...colours, ...opacity}
+
 const theme = JSON5.parse(
   fs.readFileSync(`${__dirname}/color-theme.json`, (err, data) => data)
 );
 const template = JSON.stringify(theme, null, 2)
 
-const data = mustache.render(template)
+const data = mustache.render(template, themeValues)
 
 fs.writeFileSync(path.resolve(__dirname, '..', '..','themes', 'test-color-theme.json'), data)

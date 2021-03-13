9619f83dd2f57b703c42cbbbf4722df6d2b94717
Josh Dales <joshdales@pm.me>
2021-03-13 17:40:36 -0500

    Move buildTheme up into src and update file to write to

--- src/buildTheme.js
+++ src/buildTheme.js
@@ -3,16 +3,16 @@ const path = require("path")
 const JSON5 = require("json5")
 const mustache = require("mustache")
 
-const opacity = require('./opacity')
-const colours = require('./colours.json')
+const opacity = require('./templates/opacity')
+const colours = require('./templates/colours.json')
 
 const themeValues = {...colours, opacity}
 
 const theme = JSON5.parse(
-  fs.readFileSync(`${__dirname}/color-theme.json`, (err, data) => data)
-);
+  fs.readFileSync(`${__dirname}/templates/color-theme.json`, (err, data) => data)
+)
 const template = JSON.stringify(theme, null, 2)
 
 const data = mustache.render(template, themeValues)
 
-fs.writeFileSync(path.resolve(__dirname, '..', '..','themes', 'test-color-theme.json'), data)
+fs.writeFileSync(path.resolve(__dirname, '..','themes', 'taiga-color-theme.json'), data)

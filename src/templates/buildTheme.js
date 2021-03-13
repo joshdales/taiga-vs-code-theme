c450b0067952edbfdad836965b098e7f3dd3c142
Josh Dales <joshdales@pm.me>
2021-03-13 08:00:37 -0500

    Update some formatting in buildTheme

--- src/templates/buildTheme.js
+++ src/templates/buildTheme.js
@@ -1,11 +1,12 @@
-const fs = require('fs')
-const JSON5 = require('json5')
-const mustache = require('mustache')
+const fs = require("fs")
+const JSON5 = require("json5")
+const mustache = require("mustache")
 
 const theme = JSON5.parse(
   fs.readFileSync(`${__dirname}/color-theme.json`, (err, data) => data)
-)
+);
+const template = JSON5.stringify(theme, null, 2)
 
-const template = mustache.render(JSON5.stringify(theme, null, 2))
+const data = mustache.render(template)
 
-console.log(template)
+console.log(data)

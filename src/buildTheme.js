dba4f2041f1802eee238e368b8d0b5e6d8d467fb
Josh Dales <30500472+joshdales@users.noreply.github.com>
2022-06-29 10:42:43 -0600

    Rename the theme match the type

--- src/buildTheme.js
+++ src/buildTheme.js
@@ -4,15 +4,25 @@ const JSON5 = require("json5")
 const mustache = require("mustache")
 
 const opacity = require('./templates/opacity')
-const colours = require('./templates/dark-colours.json')
+const colours = [
+  require('./templates/dark-colours.json'),
+]
 
-const themeValues = {...colours, opacity}
+function buildTheme(colourTheme) {
+  const themeValues = {...colourTheme, opacity}
 
-const theme = JSON5.parse(
-  fs.readFileSync(`${__dirname}/templates/color-theme.json`, (err, data) => data)
-)
-const template = JSON.stringify(theme, null, 2)
+  const theme = JSON5.parse(
+    fs.readFileSync(`${__dirname}/templates/color-theme.json`, (err, data) => data)
+  )
 
-const data = mustache.render(template, themeValues)
+  const template = JSON.stringify(theme, null, 2)
 
-fs.writeFileSync(path.resolve(__dirname, '..','themes', 'taiga-color-theme.json'), data)
+  const data = mustache.render(template, themeValues)
+
+  fs.writeFileSync(
+    path.resolve(__dirname, '..', 'themes', `${colourTheme.type}-color-theme.json`),
+    data
+  )
+}
+
+colours.map(buildTheme)
